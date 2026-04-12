a08c200318fb8b240b709f4908ea79f935d77da4
contactlogonova-stack <contact.logonova@gmail.com>
2026-04-12 22:23:23 +0000

    optimiser vite.config pour sécurité Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -109,4 +108,3 @@ export default defineConfig(({mode}) => {
     }
   };
 });
-
